cc227ee248aa0b1b3d71578f844a9bd7eb94aa63
Lélia Najara <lelianajara@outlook.com>
2024-03-30 17:35:04 -0300

    mais atualizado

--- Treino/num.py
+++ Treino/num.py
@@ -1,7 +1,7 @@
-"Somando"
+#Comentário
 n1=int(input('Digite um número'))
 print(type(n1))
 n2=int(input('Digite mais um número'))
 print(type(n2))
 s=n1+n2
-print('A soma é {}'.format(s))
+print('A soma entre {} e {} é {}'.format(n1, n2, s))
